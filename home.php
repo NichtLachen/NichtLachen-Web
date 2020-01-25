8a3c46042a9ad1288049078970abc11a9e7b1b2f
Linux4 <tim@linux4.de>
2020-01-25 17:08:05 +0100

    add profile.php

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once (dirname(__FILE__) . '/include/guestredirect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Nicht Lachen! | Willkommen</title>
+		<link rel="stylesheet" href="/css/stylesheet.css"/>
+	</head>
+	<body>
+		<h1 style="margin-top: 0%;">
+			<center><br>Willkommen bei Nicht Lachen 2.0!<br><br></center>
+		</h1>
+<?php
+require_once (dirname(__FILE__) . '/templates/footer.html');
+?>
+	</body>
+</html>
+
+
