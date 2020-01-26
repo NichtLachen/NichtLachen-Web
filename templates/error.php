67c615e0c0af503be9b5bf09fffe0d5d11942c47
Linux4 <tim@linux4.de>
2020-01-26 15:54:07 +0100

    remove javascript

--- templates/error.php
+++ templates/error.php
@@ -1,26 +1,9 @@
-<div class="modal" id="modal">
-	<div class="modal-content error">
-		<span class="close">&times;</span>
-		<p>Fehler: <?php echo $ERROR ?></p>
+<input type="checkbox" class="btnControl" id="btnControl"/>
+<label class="btn" for="btnControl">
+	<div class="modal">
+		<div class="modal-content error">
+			<span class="close">&times;</span>
+			<p>Fehler: <?php echo $ERROR ?></p>
+		</div>
 	</div>
-</div>
-
-<script language="javascript">
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
+</label
--- templates/success.php
+++ templates/success.php
@@ -1,26 +1,9 @@
-<div class="modal" id="modal">
-	<div class="modal-content success">
-		<span class="close">&times;</span>
-		<p><?php echo $SUCCESS ?></p>
+<input type="checkbox" class="btnControl" id="btnControl"/>
+<label class="btn" for="btnControl">
+	<div class="modal">
+		<div class="modal-content success">
+			<span class="close">&times;</span>
+			<p><?php echo $SUCCESS ?></p>
+		</div>
 	</div>
-</div>
-
-<script language="javascript">
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
+</label
