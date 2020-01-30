6a41e22047d9567ce39e710ee99ee4fffa9b8df8
Linux4 <tim@linux4.de>
2020-01-30 07:29:04 +0100

    fix scrolling to #elementid

--- templates/navbar.php
+++ templates/navbar.php
@@ -9,18 +9,32 @@ function active(string $php) : string {
 }
 
 ?>
-	<div class="topnav">
-		<font class="header">Nicht lachen!</font>
-		<div class="dropdown" style="float: right;">
-			<button class="dropbtn" style="border-radius: 50%;"><i class="fas fa-cog" style="font-size: xx-large;"></i></button>
-			<div class="dropdown-content">
-				<a<?php echo active("myprofile.php"); ?> href="myprofile.php">Mein Profil</a>
+		<div class="topnav" id="navbar">
+			<font class="header">Nicht lachen!</font>
+			<div class="dropdown" style="float: right;">
+				<button class="dropbtn" style="border-radius: 50%;"><i class="fas fa-cog" style="font-size: xx-large;"></i></button>
+				<div class="dropdown-content">
+					<a<?php echo active("myprofile.php"); ?> href="myprofile.php">Mein Profil</a>
+				</div>
 			</div>
-		</div>
-		<br><br>
+			<br><br>
 
-		<a<?php echo active("top.php"); ?> href="top.php">Top</a>
-		<a<?php echo active("new.php"); ?> href="new.php">Neu</a>
-		<a<?php echo active("categories.php"); ?> href="categories.php">Kategorien</a>
-		<a<?php echo active("abos.php"); ?> href="abos.php">Abos</a>
-	</div>
+			<a<?php echo active("top.php"); ?> href="top.php">Top</a>
+			<a<?php echo active("new.php"); ?> href="new.php">Neu</a>
+			<a<?php echo active("categories.php"); ?> href="categories.php">Kategorien</a>
+			<a<?php echo active("abos.php"); ?> href="abos.php">Abos</a>
+		</div>
+		<script language="javascript">
+			var prevScrollpos = window.pageYOffset;
+			window.onscroll = function() {
+				var currentScrollPos = window.pageYOffset;
+				if (prevScrollpos > currentScrollPos) {
+					document.getElementById("navbar").style.top = "0";
+					document.getElementById("footer").style.bottom = "0";
+				} else {
+					document.getElementById("navbar").style.top = "-1000px";
+					document.getElementById("footer").style.bottom = "-1000px";
+				}
+				prevScrollpos = currentScrollPos;
+			} 
+		</script>
