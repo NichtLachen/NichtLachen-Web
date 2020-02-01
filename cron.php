ae8b265ad4fff4098781e4467146093ba4f936cf
Linux4 <tim@linux4.de>
2020-02-01 14:01:45 +0100

    update cron.php

--- cron.php
+++ cron.php
@@ -10,9 +10,11 @@ $conn = $database->conn;
 // delete expired verify entries
 $stmt = $conn->prepare('DELETE FROM verify WHERE ExpiresAt < NOW()');
 $stmt->execute();
+echo $stmt->rowCount() . " verifications expired\n";
 
 // delete expired sessions
 $stmt = $conn->prepare('DELETE FROM sessions WHERE ExpiresAt < NOW()');
 $stmt->execute();
+echo $stmt->rowCount() . " sessions expired\n";
 
 ?>
