386d12b45487f6dc51463f2ed612d46f885580b7
Linux4 <tim@linux4.de>
2020-02-06 16:09:40 +0100

    fix

--- templates/navbar.php
+++ templates/navbar.php
@@ -10,7 +10,7 @@ $from = urlencode($_SERVER['REQUEST_URI']);
 				<button class="dropbtn" style="border-radius: 50%;"><i class="fas fa-cog" style="font-size: xx-large;"></i></button>
 				<div class="dropdown-content">
 					<a href="myprofile.php?from=<?php echo $from; ?>">Mein Profil</a>
-					<a href="myqueue.php?from=<?php echo $from; ?>">Warteschlange</a>
+					<a href="queue.php?from=<?php echo $from; ?>">Warteschlange</a>
 					<a href="myfavorites.php?from=<?php echo $from; ?>">Favoriten</a>
 					<a href="categoryfilter.php?from=<?php echo $from; ?>">Kategoriefilter</a>
 					<a href="about.php?from=<?php echo $from; ?>">Über</a>
