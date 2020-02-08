74c2367f7d062b331557725652d0616c8dfbd141
Linux4 <tim@linux4.de>
2020-02-08 17:24:56 +0100

    allow disabling of registration

--- config.php
+++ config.php
@@ -6,5 +6,6 @@ define ('DBHOST', 'localhost');
 define ('DBNAME', 'nichtlachen_dev');
 define ('POSTS_PER_PAGE', '30');
 define ('ALLOWED_IMAGE_EXTENSIONS', array('png', 'jpg', 'jpeg'));
+define ('ALLOW_REGISTRATION', true);
 
 ?>
--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 
 require_once (__DIR__ . '/include/loginredirect.php');
 require_once (__DIR__ . '/classes/db/DatabaseAPI.php');
+require_once (__DIR__ . '/config.php');
 
 function sendVerifyMail(string $username, string $email, string $key) {
 	$headers = array("Content-Type: text/plain; charset=UTF-8");
@@ -24,62 +25,67 @@ function sendVerifyMail(string $username, string $email, string $key) {
 	</head>
 	<body>
 <?php
-if (isset($_POST['register'])) {
-	if(isset ($_POST['username']) && !empty($_POST['username'])) {
-		$username = $_POST['username'];
+if (ALLOW_REGISTRATION) {
+	if (isset($_POST['register'])) {
+		if(isset ($_POST['username']) && !empty($_POST['username'])) {
+			$username = $_POST['username'];
 
-		if(isset($_POST['email']) && !empty($_POST['email'])) {
-			$email = $_POST['email'];
-			$pwlen = isset($_POST['password']) ? strlen($_POST['password']) : 0;
+			if(isset($_POST['email']) && !empty($_POST['email'])) {
+				$email = $_POST['email'];
+				$pwlen = isset($_POST['password']) ? strlen($_POST['password']) : 0;
 
-			if($pwlen >= 8) {
-				$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-				$api = new DatabaseAPI();
+				if($pwlen >= 8) {
+					$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+					$api = new DatabaseAPI();
 
-				if(!$api->isNameInUse($username) && !$api->isNameInVerification($username)) {
-					if (strpos($username, ' ') == false) {
-						$user = $api->getUserByEMail($email);
+					if(!$api->isNameInUse($username) && !$api->isNameInVerification($username)) {
+						if (strpos($username, ' ') == false) {
+							$user = $api->getUserByEMail($email);
 
-						if($user == null && !$api->isEMailInVerification($email)) {
-							$vid = $api->verify($username, $email, $password);
-							sendVerifyMail($username, $email, $vid);
-							$SUCCESS = "Eine Bestätigungsemail wurde an die angegebene EMail-Adresse gesendet, klicken Sie auf den Link in der EMail um Ihren Account zu aktivieren!";
-							require (__DIR__ . '/templates/success.php');
+							if($user == null && !$api->isEMailInVerification($email)) {
+								$vid = $api->verify($username, $email, $password);
+								sendVerifyMail($username, $email, $vid);
+								$SUCCESS = "Eine Bestätigungsemail wurde an die angegebene EMail-Adresse gesendet, klicken Sie auf den Link in der EMail um Ihren Account zu aktivieren!";
+								require (__DIR__ . '/templates/success.php');
+							} else {
+								$ERROR = "Diese EMail-Adresse wird bereits verwendet!";
+								require (__DIR__ . '/templates/error.php');
+							}
 						} else {
-							$ERROR = "Diese EMail-Adresse wird bereits verwendet!";
+							$ERROR = "Benutzernamen dürfen keine Leerzeichen enthalten!";
 							require (__DIR__ . '/templates/error.php');
 						}
 					} else {
-						$ERROR = "Benutzernamen dürfen keine Leerzeichen enthalten!";
+						$ERROR = "Dieser Benutzername wird bereits verwendet!";
 						require (__DIR__ . '/templates/error.php');
 					}
 				} else {
-					$ERROR = "Dieser Benutzername wird bereits verwendet!";
+					$ERROR = "Das Passwort muss mindestens 8 Zeichen lang sein!";
 					require (__DIR__ . '/templates/error.php');
 				}
 			} else {
-				$ERROR = "Das Passwort muss mindestens 8 Zeichen lang sein!";
+				$ERROR = "EMail-Adresse darf nicht leer sein!";
 				require (__DIR__ . '/templates/error.php');
 			}
 		} else {
-			$ERROR = "EMail-Adresse darf nicht leer sein!";
+			$ERROR = "Benutzername darf nicht leer sein!";
 			require (__DIR__ . '/templates/error.php');
 		}
-	} else {
-		$ERROR = "Benutzername darf nicht leer sein!";
-		require (__DIR__ . '/templates/error.php');
-	}
-} else if(isset($_GET['key'])) {
-	$api = new DatabaseAPI();
+	} else if(isset($_GET['key'])) {
+		$api = new DatabaseAPI();
 	
-	if($api->verifyEnable($_GET['key'])) {
-		$SUCCESS = 'Ihr Account wurde erfolgreich aktiviert! <div class="container"><a href="login.php" class="button">Anmelden</a></div>';
-		require (__DIR__ . '/templates/success.php');
-	} else {
-		$ERROR = "Die angegebene Verifikations-ID existiert nicht! Ist Ihre Registrierung abgelaufen?";
-		require (__DIR__ . '/templates/error.php');
-	}
-} 
+		if($api->verifyEnable($_GET['key'])) {
+			$SUCCESS = 'Ihr Account wurde erfolgreich aktiviert! <div class="container"><a href="login.php" class="button">Anmelden</a></div>';
+			require (__DIR__ . '/templates/success.php');
+		} else {
+			$ERROR = "Die angegebene Verifikations-ID existiert nicht! Ist Ihre Registrierung abgelaufen?";
+			require (__DIR__ . '/templates/error.php');
+		}
+	} 
+} else {
+	$ERROR = "Die Registrierung ist zurzeit deaktiviert";
+	require (__DIR__ . '/templates/error.php');
+}
 
 $TITLE = "Registrieren";
 require_once (__DIR__ . '/templates/navbar_back.php');
