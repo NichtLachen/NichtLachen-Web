9fdc6f21dae06edf9feb77ff9b6c5162309d5be6
Linux4 <tim@linux4.de>
2020-02-08 08:43:09 +0100

    allow posting

--- post.php
+++ post.php
@@ -4,6 +4,7 @@ require_once (__DIR__ . '/include/guestredirect.php');
 require_once (__DIR__ . '/classes/db/DatabaseAPI.php');
 
 $api = new DatabaseAPI();
+$uid = $api->getUIDBySessionID(session_id());
 
 $from = isset($_GET['from']) ? $_GET['from'] : "./";
 
@@ -22,21 +23,31 @@ $from = isset($_GET['from']) ? $_GET['from'] : "./";
 $TITLE = "Witz hinzufügen";
 require_once (__DIR__ . '/templates/navbar_back.php');
 
-if (isset($_GET['cid'])) {
+if (isset($_GET['cid']) && !isset($_POST['cid'])) {
 	$name = $api->getCategoryName($_GET['cid']);
 
 	if ($name != null && !$api->isSuperCategory($_GET['cid'])) {
 		
 ?>
-		<form method="POST" action="">
-			
+		<form class="newpost" method="POST" action="" id="post">
+			<input type="hidden" name="cid" value="<?php echo $_GET['cid']; ?>">
+			<textarea form="post" name="text"></textarea><br>
+			<br>
+			<input type="submit" class="button">
 		</form>
 <?php
 	} else {
 		$ERROR = "Kategorie nicht gefunden!";
 		require (__DIR__ . '/templates/error.php');
 	}
-} else if (isset($_POST['cid'])) {
+} else if (isset($_POST['cid']) && isset($_POST['text'])) {
+	$text = $_POST['text'];
+	if (!empty($text)) {
+		$api->postVerify($_POST['cid'], $uid, $text);
+	}
+
+	header("302 Found");
+	header("Location: " . $from);
 } else {
 	$ERROR = "Ungültige Anfrage";
 	require (__DIR__ . '/templates/error.php');
