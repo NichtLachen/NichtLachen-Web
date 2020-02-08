ef657f8c08091509adc271b1df2444238c10ac4b
Linux4 <tim@linux4.de>
2020-02-08 13:52:26 +0100

    style improvement

--- templates/comment.php
+++ templates/comment.php
@@ -14,6 +14,8 @@ if ($comment->getReplyTo() != null) {
 	$replyTo = $api->getUserByUID($comment->getReplyTo());
 	$to = "<a class=\"post-category\" href=\"users.php?uid=" . $replyTo->getUID() . "&from=" . $from . "\">@" . $replyTo->getName() . " </a>";
 }
+
+$color = $api->isCommentLikeSet($comment->getCMTID(), $uid, 1) ? "red" : "grey";
 ?>
 		<div class="post" id="<?php echo $comment->getCMTID();?>">
 			<a class="post-category" href="users.php?uid=<?php echo $user->getUID();?>&from=<?php echo $from; ?>"><?php echo $user->getName(); ?></a>
@@ -21,5 +23,5 @@ if ($comment->getReplyTo() != null) {
 			<br><br>
 			<div class="post-content"><?php echo $to; ?><?php echo escapeHTML($comment->getContent()); ?></div>
 			<br>
-			<div class="post-dislike"><a href="like.php?like=1&cmtid=<?php echo $comment->getCMTID();?>&from=<?php echo $from; ?>"><i class="far fa-heart"></i></a> <?php echo $api->countCommentLikes($comment->getCMTID());?></div>
+			<div class="post-like"><a href="like.php?like=1&cmtid=<?php echo $comment->getCMTID();?>&from=<?php echo $from; ?>"><i style="color: <?php echo $color; ?>" class="fas fa-heart"></i></a> <?php echo $api->countCommentLikes($comment->getCMTID());?></div>
 		</div>
