03ea7b0070ff5c086a607780d3c1f2ec8f91ecd1
Linux4 <tim@linux4.de>
2020-02-10 19:07:46 +0100

    do not minify css (does not make it very much smaller ...)

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Nicht Lachen! | <?php echo $TITLE; ?></title>
-		<link rel="stylesheet" href="css/stylesheet.min.css"/>
+		<link rel="stylesheet" href="css/stylesheet.css"/>
 		<script language="javascript" src="js/main.js"></script>
 		<meta name='viewport' content="width=device-width, initial-scale=1" />
 	</head>
