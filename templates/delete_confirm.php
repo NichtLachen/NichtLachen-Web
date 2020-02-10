3d6021a6a73fd6b3b2a3fa5ae5ddb77d90c28057
Linux4 <tim@linux4.de>
2020-02-10 18:55:41 +0100

    format code

--- templates/delete_confirm.php
+++ templates/delete_confirm.php
@@ -1,15 +1,15 @@
-<div>
-	<script language="javascript">
-		function reset<?php echo $delid; ?>() {
-			document.getElementById('delete<?php echo $delid; ?>').checked = false;
-			return false;
-		}
-	</script>
-	<div class="modal">
-		<div class="modal-content error">
-			<a style="text-decoration: none;" class="close" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">&times;</a>
-			<p>Soll Beitrag #<?php echo $delid; ?> wirklich gelöscht werden?</p>
-			<a class="button success" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">Nein</a> <a class="button error" href="<?php echo $delete; ?>">Ja</a>
-		</div>
-	</div>
-</div>
+			<div>
+				<script language="javascript">
+					function reset<?php echo $delid; ?>() {
+						document.getElementById('delete<?php echo $delid; ?>').checked = false;
+						return false;
+					}
+				</script>
+				<div class="modal">
+					<div class="modal-content error">
+						<a style="text-decoration: none;" class="close" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">&times;</a>
+						<p>Soll Beitrag #<?php echo $delid; ?> wirklich gelöscht werden?</p>
+						<a class="button success" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">Nein</a> <a class="button error" href="<?php echo $delete; ?>">Ja</a>
+					</div>
+				</div>
+			</div>
