6134e8adc1397a119252a6d1b9b8e247f51da9db
Linux4 <tim@linux4.de>
2020-02-10 16:46:06 +0100

    improvements to delete_confirm.php (make it usable when javascript is unsupported)

--- templates/delete_confirm.php
+++ templates/delete_confirm.php
@@ -1,10 +1,16 @@
 <div>
-	<!--<input type="checkbox" class="btnControl" id="btnControl<?php echo $delid; ?>"/>-->
+	<script language="javascript">
+		function reset<?php echo $delid; ?>() {
+			document.getElementById('delete<?php echo $delid; ?>').checked = false;
+			document.getElementbyId('btnControl<?php echo $delid; ?>').checked = false;
+			return false;
+		}
+	</script>
 	<div class="modal">
 		<div class="modal-content error">
-			<label id="btnControlL<?php echo $delid; ?>" class="close" for="btnControl<?php echo $delid; ?>" onclick="document.getElementById('delete<?php echo $delid; ?>').checked = false;">&times;</label>
+			<a style="text-decoration: none;" class="close" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">&times;</a>
 			<p>Soll Beitrag #<?php echo $delid; ?> wirklich gelöscht werden?</p>
-			<a class="button error" href="<?php echo $delete; ?>">Ja</a>
+			<a class="button success" href="<?php echo urldecode($from); ?>" onclick="return reset<?php echo $delid; ?>();">Nein</a> <a class="button error" href="<?php echo $delete; ?>">Ja</a>
 		</div>
 	</div>
 </div>
