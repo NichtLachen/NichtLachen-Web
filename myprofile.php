a6e38dad8d098d13d0b2f4b040c516e6cf746da6
Linux4 <tim@linux4.de>
2020-02-11 17:39:24 +0100

    do not try to load profileimage if it does not exist

--- myprofile.php
+++ myprofile.php
@@ -12,9 +12,12 @@ $TITLE = "Mein Profil";
 
 require_once (__DIR__ . '/templates/header.php');
 require_once (__DIR__ . '/templates/profilenavbar.php');
+
+$profileimage = findProfileImage($uid);
+$bgImage = $profileImage != null ? "url('profileimages/" . $profileimage . "')" : "none"; 
 ?>
 		<center>
-		<div class="profileimage" style="background-image: url('profileimages/<?php echo findProfileImage($uid); ?>');">
+		<div class="profileimage" style="background-image: <?php echo $bgImage; ?>;">
 		</div>
 		<p style="font-size: x-large; font-weight: bold;"><?php echo $user->getName(); ?> <a class="edit" href="editprofile.php?from=<?php echo urlencode($_SERVER['REQUEST_URI']); ?>"><i class="fas fa-edit"></i></a></p>
 		<br>
--- users.php
+++ users.php
@@ -13,9 +13,12 @@ $TITLE = $user != null ? "Profil von " . $user->getName() : "Benutzer nicht gefu
 
 require_once (__DIR__ . '/templates/header.php');
 require_once (__DIR__ . '/templates/navbar_back.php');
+
+$profileimage = findProfileImage($uid);
+$bgImage = $profileImage != null ? "url('profileimages/" . $profileimage . "')" : "none"; 
 ?>
 		<center>
-		<div class="profileimage" style="background-image: url('profileimages/<?php echo findProfileImage($uid); ?>');">
+		<div class="profileimage" style="background-image: <?php echo $bgImage; ?>;">
 		</div>
 		<p style="font-size: x-large; font-weight: bold;"><?php echo $user != null ? $user->getName() : ""; ?></p>
 <?php
