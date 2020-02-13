0b16bad93594dac471b0b6acea8e369170fa304e
Linux4 <tim@linux4.de>
2020-02-13 14:12:27 +0100

    preload js and css

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Nicht Lachen! | <?php echo $TITLE; ?></title>
+		<link rel="preload" href="css/stylesheet.css" as="style"/>
+		<link rel="preload" href="js/nichtlachen.js" as="script"/>
 		<link rel="stylesheet" href="css/stylesheet.css"/>
 		<script language="javascript" src="js/nichtlachen.js"></script>
 		<script language="javascript">
