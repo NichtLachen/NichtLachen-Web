c88b4826a4558324a9d883474b5efb7241c70110
Linux4 <tim@linux4.de>
2020-02-17 20:12:25 +0100

    fix

--- templates/user.php
+++ templates/user.php
@@ -5,6 +5,7 @@ require_once (__DIR__ . '/../include/htmlutils.php');
 require_once (__DIR__ . '/../include/stringutils.php');
 
 $api = new DatabaseAPI();
+$myuid = $api->getUIDBySessionID(session_id());
 
 $lastid = isset($last_user) ? $last_user->getUID() : 0; // 0 does never exist
 $last_user = $user;
@@ -19,5 +20,11 @@ $content = splitTextAtLength(escapeHTML($user->getDescription()), 800);
 			<br><br>
 			<div class="post-content"><?php echo $content[0]; if (!empty($content[1])) { ?><input type="checkbox" class="showMore" id="showMore<?php echo $user->getUID(); ?>"><label for="showMore<?php echo $user->getUID();?>" id="showMoreL<?php echo $user->getUID(); ?>">Mehr anzeigen <i class="fa fa-arrow-down" aria-hidden="true"></i></label><div for="showMoreL<?php echo $user->getUID(); ?>"><?php echo $content[1]; ?></div><?php } ?></div>
 			<br>
+<?php
+if ($user->getUID() != $myuid) {
+?>
 			<a class="button" onclick="return callURLWithReload('subscribe.php?uid=<?php echo $user->getUID();?>');" href="subscribe.php?uid=<?php echo $user->getUID();?>&from=<?php echo $from_delete; ?>">Abonniert</a>
+<?php
+}
+?>
 		</div>
