c1e577c6e1a3a64ca9c949228aa5c9f1cb0129e0
Tim Zimmermann <tim@linux4.de>
2020-02-17 20:33:47 +0100

    some fixes

--- templates/profilenavbar.php
+++ templates/profilenavbar.php
@@ -17,15 +17,20 @@ $TITLE = $uid == $myuid ? "Mein Profil" : $TITLE;
 
 ?>
 		<div class="topnav">
-			<a style="margin-left: 20px;" href="<?php echo $from; ?>"><i class="fa fa-arrow-left"></i></a>
+			<a onclick="return back();" style="margin-left: 20px;" href="<?php echo $from; ?>"><i class="fa fa-arrow-left"></i></a>
 			<div class="header"><?php echo $TITLE; ?></div>
+<?php
+if($uid == $myuid) {
+?>
 			<a style="float: right" href="logout.php">Ausloggen</a>
+<?php
+}
+?>
 			<br><br>
 
 			<a<?php echo active("users.php");?> href="users.php?uid=<?php echo $uid; ?>&from=<?php echo urlencode($from); ?>">Profil</a>
 			<a<?php echo active("user_posts.php");?> href="user_posts.php?uid=<?php echo $uid; ?>&from=<?php echo urlencode($from); ?>">Beiträge</a>
 <?php
-
 if($uid == $myuid) {
 ?>
 			<a<?php echo active("myqueue.php");?> href="myqueue.php?from=<?php echo urlencode($from); ?>">Eingereicht</a>
