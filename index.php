9c7c8c774f504a34f26d672a2b28c08582f7cde9
Tim Zimmermann <tim@linux4.de>
2020-03-15 11:01:32 +0100

    some small style fixes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once (__DIR__ . '/templates/header.php');
 		<h1 class="h1" style="margin-top: 0%;">
 			<center><br>Willkommen bei Nicht Lachen 2.0!<br><br></center>
 		</h1>
-		<div class="center" style="font-family: Arial; background-color: var(--colorSecondaryBackground); color: var(--colorSecondaryText); border-radius: 8px; width: 15em; font-size: x-large; margin-top: 6em; padding: 1em;">
+		<div class="center" style="font-family: Arial; background-color: #ff9900; color: var(--colorSecondaryText); border-radius: 8px; width: 15em; font-size: x-large; margin-top: 6em; padding: 1em;">
 			Melden Sie sich an:<br>
 			<br>
 			<a href="login.php" class="button">Anmelden</a>
