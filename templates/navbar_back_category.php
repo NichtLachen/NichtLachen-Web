a0cea08aaffd0ef4d3b295414dbf3ad276e56f92
Tim Zimmermann <tim@linux4.de>
2020-03-19 07:22:36 +0100

    fix positioning with float using margin-top: -0.5em

--- templates/navbar_back_category.php
+++ templates/navbar_back_category.php
@@ -7,7 +7,7 @@ $myfrom = urlencode($_SERVER['REQUEST_URI']);
 ?>
 		<div class="topnav">
 			<a onclick="return back();" style="margin-left: 1.5em;" href="<?php echo $from; ?>"><i class="fa fa-arrow-left"></i></a>
-			<a style="float: right; margin-right: 20px;" href="post.php?cid=<?php echo $_GET['cid']; ?>&from=<?php echo $myfrom; ?>"><i class="fas fa-plus"></i></a>
 			<div class="header"><?php echo $TITLE; ?></div>
+			<a style="float: right; margin-right: 1.5em; margin-top: -0.5em" href="post.php?cid=<?php echo $_GET['cid']; ?>&from=<?php echo $myfrom; ?>"><i class="fas fa-plus"></i></a>
 			<br><br>
 		</div>
