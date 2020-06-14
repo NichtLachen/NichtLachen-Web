93e4df27a542261477a26beb7363857370db8d3c
Tim Zimmermann <tim@linux4.de>
2020-06-14 16:54:39 +0200

    templates: error/success: replace modal with sweetalert2

--- templates/error.php
+++ templates/error.php
@@ -1,10 +1,7 @@
-<?php
-$randId = "btnControl" . rand(0, PHP_INT_MAX);
-?>
-<input type="checkbox" class="btnControl" id="<?php echo $randId; ?>"/>
-<div class="modal">
-	<div class="modal-content error">
-		<label class="close" for="<?php echo $randId; ?>">&times;</label>
-		<p>Fehler: <?php echo $ERROR ?></p>
-	</div>
-</div>
+<script language="javascript">
+Swal.fire({
+	title: "Fehler",
+	text: "<?php echo $ERROR; ?>",
+	icon: 'error'
+});
+</script>
--- templates/success.php
+++ templates/success.php
@@ -1,10 +1,7 @@
-<?php
-$randId = "btnControl" . rand(0, PHP_INT_MAX);
-?>
-<input type="checkbox" class="btnControl" id="<?php echo $randId; ?>"/>
-<div class="modal">
-	<div class="modal-content success">
-		<label class="close" for="<?php echo $randId; ?>">&times;</label>
-		<p><?php echo $SUCCESS ?></p>
-	</div>
-</div>
+<script language="javascript">
+Swal.fire({
+	title: "Erfolg",
+	text: "<?php echo $SUCCESS; ?>",
+	icon: 'success'
+});
+</script>
